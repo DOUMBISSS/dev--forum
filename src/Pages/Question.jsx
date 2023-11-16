f307dd5773e61f16dafbc63a06aecbe9e075e126
DOUMBISSS <104175220+DOUMBISSS@users.noreply.github.com>
2023-11-16 22:55:17 +0000

    maj

--- src/Pages/Question.jsx
+++ src/Pages/Question.jsx
@@ -150,7 +150,7 @@ export default function Question (){
 
             </div>
                                     </div>
-                                <Footer/>
+                                {/* <Footer/> */}
         </div>
     )
 }
